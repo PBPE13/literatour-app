9d640abf63e003c443e7ed4dc5bb3c10464140f4
JamesTumbelaka <jamesztumbelaka@gmail.com>
2023-12-12 10:08:45 +0700

    displayed books

--- lib/home.dart
+++ lib/home.dart
@@ -14,17 +14,15 @@ class DetailBookPage extends StatefulWidget {
 
 class _DetailBookPageState extends State<DetailBookPage> {
   Future<List<Book>> fetchProduct() async {
-      var url = Uri.parse(
-          'https://literatour-e13-tk.pbp.cs.ui.ac.id/json/');
+      var url = Uri.parse('https://literatour-e13-tk.pbp.cs.ui.ac.id/api/books/api/book/');
       var response = await http.get(
           url,
-          headers: {"Content-Type": "application/json"},
+          headers: {"Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"},
       );
 
-      // melakukan decode response menjadi bentuk json
       var data = jsonDecode(utf8.decode(response.bodyBytes));
 
-      // melakukan konversi data json menjadi object Product
       List<Book> listBook = [];
       for (var d in data) {
           if (d != null) {
@@ -35,6 +33,16 @@ class _DetailBookPageState extends State<DetailBookPage> {
   }
   @override
   Widget build(BuildContext context) {
+      double screenWidth = MediaQuery.of(context).size.width;
+
+        int crossAxisCount;
+        if (screenWidth > 1200) {
+            crossAxisCount = 3;
+        } else if (screenWidth > 600) {
+            crossAxisCount = 2;
+        } else {
+            crossAxisCount = 1;
+        }
       return Scaffold(
           appBar: AppBar(
           title: const Text('Literatour', 
@@ -49,33 +57,73 @@ class _DetailBookPageState extends State<DetailBookPage> {
               builder: (context, AsyncSnapshot snapshot) {
                   if (snapshot.data == null) {
                       return const Center(child: CircularProgressIndicator());
-                  } else {
-                      if (!snapshot.hasData) {
-                      return const Column(
-                          children: [
-                          Text(
+                  } else if (!snapshot.hasData) {
+                      return const Center(
+                          child: Text(
                               "Tidak ada data produk.",
-                              style:
-                                  TextStyle(color:Color(0xFF333333), fontSize: 20),
+                              style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
                           ),
-                          SizedBox(height: 8),
-                          ],
                       );
                   } else {
-                      return ListView.builder(
-                          itemCount: snapshot.data!.length,
-                          itemBuilder: (_, index) => Card(
-                            color:Colors.white,
-                            elevation: 4.0,
-                            margin: const EdgeInsets.all(8.0),
-                           
+                      return GridView.builder(
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: crossAxisCount,
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10,
+                              childAspectRatio: 1 / 1.1,
                           ),
-                        );
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (_, index) {
+                              Book book = snapshot.data![index];
+                              return InkWell(
+                                  child: Card(
+                                    shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                      child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                                          children: [
+                                              ClipRRect(
+                                                  borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)), // Rounded corners for the top of the image
+                                                  child: AspectRatio(
+                                                      aspectRatio: 1.5,
+                                                      child: Image.network(
+                                                          'https://images.saymedia-content.com/.image/t_share/MTc0MjYyNTc5Mjg2MjU1MTAw/10-books-over-1000-pages-to-read.jpg',
+                                                          fit: BoxFit.cover,
+                                                      ),
+                                                  ),
+                                              ),
+                                              Padding(
+                                                  padding: const EdgeInsets.all(10.0),
+                                                  child: SingleChildScrollView(
+                                                      child: Column(
+                                                          mainAxisAlignment: MainAxisAlignment.start,
+                                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                                          children: [
+                                                              Text(
+                                                                  "${snapshot.data![index].fields.title}",
+                                                                  style: const TextStyle(
+                                                                      fontSize: 20.0,
+                                                                      fontWeight: FontWeight.bold,
+                                                                  ),
+                                                              ),
+                                                              const SizedBox(height: 5),
+                                                              Text("${snapshot.data![index].fields.authors}"),
+                                                              const SizedBox(height: 5),
+                                                              Text("${snapshot.data![index].fields.averageRating}")
+                                                          ],
+                                                      ),
+                                                  ),
+                                              ),
+                                          ],
+                                      ),
+                                  ),
+                              );
+                          },
+                      );
                   }
-              }
-          }) 
-          ,bottomNavigationBar: BottomMenu(0),
-          );
-          
+              },
+          ),bottomNavigationBar: BottomMenu(0),
+        );  
       }
 }
