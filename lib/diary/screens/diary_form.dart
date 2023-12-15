40199c9995fb28f2fc8e083f8b9bc8f0274b27f5
Raisa Diandra Survijanto <raisa.diandra@yahoo.com>
2023-12-16 00:16:04 +0700

    added diary models and page

---
+++ lib/diary/screens/diary_form.dart
@@ -0,0 +1,164 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:literatour_app/main.dart';
+import 'package:literatour_app/diary/screens/diary_page.dart';
+import 'package:literatour_app/widgets/bottom_menu.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+
+class DiaryFormPage extends StatefulWidget {
+  const DiaryFormPage({super.key});
+
+  @override
+  State<DiaryFormPage> createState() => _DiaryFormPageState();
+}
+
+class _DiaryFormPageState extends State<DiaryFormPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _title = "";
+  DateTime? _finishDate = DateTime.now();
+  String _notes = "";
+
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Add Diary',
+          ),
+        ),
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+      ),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Book Title",
+                  labelText: "Book Title",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (String? value) {
+                  setState(() {
+                    _title = value!;
+                  });
+                },
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Title cannot be empty!";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Finish Date",
+                  labelText: "Finish Date",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (String? value) {
+                  setState(() {
+                    _finishDate = DateTime.parse(value!);
+                  });
+                },
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Finish date cannot be empty!";
+                  }
+                  if (DateTime.tryParse(value) == null) {
+                    return "Finish date cannot be empty!";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Notes",
+                  labelText: "Notes",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (String? value) {
+                  setState(() {
+                    _notes = value!;
+                  });
+                },
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Notes cannot be empty!";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.indigo),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      // Kirim ke Django dan tunggu respons
+                      final response = await request.postJson(
+                          "https://raisa-diandra-tugas.pbp.cs.ui.ac.id/create-flutter/",
+                          // "http://10.0.2.2/create-flutter/",
+                          jsonEncode(<String, String>{
+                            'title': _title,
+                            'finishDate': _finishDate.toString(),
+                            'notes': _notes,
+                          }));
+                      if (response['status'] == 'success') {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text("Produk baru berhasil disimpan!"),
+                        ));
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => DiaryPage()),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content:
+                              Text("Terdapat kesalahan, silakan coba lagi."),
+                        ));
+                      }
+                    }
+                  },
+                  child: const Text(
+                    "Save",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+      bottomNavigationBar: BottomMenu(3),
+    );
+  }
+}
